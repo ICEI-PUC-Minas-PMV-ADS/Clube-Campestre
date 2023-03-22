9d433052f09762ff2353f4965fbc260cc107df3c
Carlos Teixeira <carlos.teixeira@sga.pucminas.br>
2023-03-22 20:21:08 -0300

    projeto

---
+++ src/Back End/apis-web-services-clube-campestre/WeatherForecast.cs
@@ -0,0 +1,11 @@
+namespace apis_web_services_clube_campestre {
+    public class WeatherForecast {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
