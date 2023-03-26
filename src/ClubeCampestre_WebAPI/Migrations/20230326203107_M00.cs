355607e781b359aac1f5b4be4d8844a8bb315aec
GTadim <70529816+GTadim@users.noreply.github.com>
2023-03-26 17:37:53 -0300

    Adição da primeira migration e criação dass tabelas socios e mensalidades no banco de dados

---
+++ src/ClubeCampestre_WebAPI/Migrations/20230326203107_M00.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubeCampestre_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class M00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "socios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cota = table.Column<int>(type: "int", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Identidade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Cep = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Logradouro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefonePrincipal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneSecundario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeAssociacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Condicao = table.Column<int>(type: "int", nullable: false),
+                    SituacaoFinanceira = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_socios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "mensalidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MesAnoReferencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeVencimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    DataDePagamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorPago = table.Column<float>(type: "real", nullable: false),
+                    SocioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mensalidades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_mensalidades_socios_SocioId",
+                        column: x => x.SocioId,
+                        principalTable: "socios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mensalidades_SocioId",
+                table: "mensalidades",
+                column: "SocioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mensalidades");
+
+            migrationBuilder.DropTable(
+                name: "socios");
+        }
+    }
+}
---
+++ src/ClubeCampestre_WebAPI/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClubeCampestre_WebAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Socio> Socios { get; set; }
+        public DbSet<Mensalidade> Mensalidades { get; set; }
+    }
+}
