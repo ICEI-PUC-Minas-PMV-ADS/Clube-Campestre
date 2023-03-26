703c5237ba78755d14d7e49393f702d066a66f12
GTadim <70529816+GTadim@users.noreply.github.com>
2023-03-26 17:00:48 -0300

    Adição da Model Socio

---
+++ src/ClubeCampestre_WebAPI/Models/Socio.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ClubeCampestre_WebAPI.Models
+{
+    public class Socio
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Cota { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Cpf { get; set; }
+        public string Identidade { get; set; }
+        [Required]
+        public DateTime DataDeNascimento { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string TelefonePrincipal { get; set; }
+        public string TelefoneSecundario { get; set; }
+        [Required]
+        public DateTime DataDeAssociacao { get; set; }
+        [Required]
+        public CondicaoDoSocio CondicaoDoSocio { get; set; }
+        [Required]
+        public SituacaoFinanceira SituacaoFinanceira { get; set; }
+
+
+    }
+    public enum CondicaoDoSocio
+    {
+        [Display(Name = "Fundador")]
+        Fundador,
+        [Display(Name = "Proprietario")]
+        Proprietario,
+        [Display(Name = "Temporario")]
+        Temporario,
+        [Display(Name = "Inativo")]
+        Inativo
+    }
+
+    public enum SituacaoFinanceira
+    {
+        [Display(Name = "Regular")]
+        Regular,
+        [Display(Name = "Debito")]
+        Debito,
+        [Display(Name = "Inadimplente")]
+        Inadimplente
+    }
+}
