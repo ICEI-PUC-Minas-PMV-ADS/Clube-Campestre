1133e75f348c8564996b74cffc219d49b1c9e9e0
GTadim <70529816+GTadim@users.noreply.github.com>
2023-03-26 23:19:01 -0300

    Criação da pasta de FrontEnd

---
+++ src/ClubeCampestre_WebAPI/Migrations/20230327021035_M03.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubeCampestre_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class M03 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Uf",
+                table: "socios",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uf",
+                table: "socios");
+        }
+    }
+}
