d7cfd178cc0fd13caa3bb100dbdd698731a7b1a7
GTadim <70529816+GTadim@users.noreply.github.com>
2023-04-09 20:07:54 -0300

    UsuarioDto

---
+++ src/ClubeCampestre_WebAPI/Models/UsuarioDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClubeCampestre_WebAPI.Models {
+    public class UsuarioDto {
+        public int? Id { get; set; }
+        [Required]
+        public string CodigoUsuario { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string CPF { get; set; }
+        [Required]
+        public string Senha { get; set; }
+        [Required]
+        public TipoUsuario TipoUsuario { get; set; }
+
+    }
+}
