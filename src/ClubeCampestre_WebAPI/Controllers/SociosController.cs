cb75a1b854c80c2eedeb6564ddd712da5ff5f412
GTadim <70529816+GTadim@users.noreply.github.com>
2023-04-09 20:09:28 -0300

    Add Dependentes e Mensalidades ao Socio

--- src/ClubeCampestre_WebAPI/Controllers/SociosController.cs
+++ src/ClubeCampestre_WebAPI/Controllers/SociosController.cs
@@ -35,6 +35,8 @@ namespace ClubeCampestre_WebAPI.Controllers
         public async Task<ActionResult> ListarSociosAtivos()
         {
             var socios = await _context.Socios
+            .Include(t => t.Dependentes)
+            .Include(m => m.Mensalidades)
             .Where(s => s.Condicao != CondicaoDoSocio.Inativo)
             .ToListAsync();
 
@@ -45,6 +47,7 @@ namespace ClubeCampestre_WebAPI.Controllers
         public async Task<ActionResult> ListarSocioPorCota(int cota)
         {
             var socio = await _context.Socios
+            .Include(t => t.Dependentes)
             .FirstOrDefaultAsync(s => s.Cota == cota);  
 
             if (socio == null) return NotFound();
