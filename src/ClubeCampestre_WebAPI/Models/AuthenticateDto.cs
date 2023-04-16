f23f6563fda435fb30f29a7b1e1fda5417c8ce51
GTadim <70529816+GTadim@users.noreply.github.com>
2023-04-16 16:23:05 -0300

    Ajust AutenticateDto

---
+++ src/ClubeCampestre_WebAPI/Models/AuthenticateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClubeCampestre_WebAPI.Models {
+    public class AuthenticateDto {
+
+        [Required]
+        public string CodigoUsuario { get; set; }
+        [Required]
+        public string Senha { get; set; }
+    }
+}
