c0ecfc152ca371f6fc82451a4e5ca83115914710
Carlos Teixeira <carlos.teixeira@sga.pucminas.br>
2023-05-15 20:15:25 -0300

    merge

--- src/ClubeCampestre_WebAPI/Models/AuthenticateDto.cs
+++ src/ClubeCampestre_WebAPI/Models/AuthenticateDto.cs
@@ -4,7 +4,7 @@ namespace ClubeCampestre_WebAPI.Models {
     public class AuthenticateDto {
 
         [Required]
-        public string CodigoUsuario { get; set; }
+        public int CodigoUsuario { get; set; }
         [Required]
         public string Senha { get; set; }
     }
