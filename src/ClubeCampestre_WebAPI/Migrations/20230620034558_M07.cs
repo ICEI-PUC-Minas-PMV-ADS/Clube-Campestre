6f343baf985d466df057be864ce5357f69f1495f
GTadim <70529816+GTadim@users.noreply.github.com>
2023-06-20 00:48:10 -0300

    Update coluna ValorPorDependente to ValorDoConvite na base ParametrosFinanceiros

---
+++ src/ClubeCampestre_WebAPI/Migrations/20230620034558_M07.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubeCampestre_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class M07 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ValorPorDependente",
+                table: "parametros_financeiros",
+                newName: "ValorDoConvite");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ValorDoConvite",
+                table: "parametros_financeiros",
+                newName: "ValorPorDependente");
+        }
+    }
+}
